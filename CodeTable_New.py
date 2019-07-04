13940cc02abc69ba78322b4d880dca92eb2db7f8
Hans <jjabakker@icloud.com>
2019-07-04 07:20:01 +0200

    Making a new start point for the day

--- CodeTable_New.py
+++ CodeTable_New.py
@@ -2,84 +2,126 @@ import pprint
 from Bio.Alphabet import IUPAC
 from Bio.Seq import Seq, MutableSeq
 
-GeneticCode = {"UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L",
-               "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
-               "UAU": "Y", "UAC": "Y", "UAA": "STOP", "UAG": "STOP",
-               "UGU": "C", "UGC": "C", "UGA": "STOP", "UGG": "W",
-               "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
-               "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
-               "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
-               "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
-               "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
-               "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
-               "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K",
-               "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
-               "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
-               "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
-               "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
-               "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"}
 
-FromThreeToOneTable = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
-                       'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
-                       'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
-                       'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
+class CodeTable:
 
-FromOneToThreeTable = {'C': 'CYS', 'D': 'ASP', 'S': 'SER', 'Q': 'GLN', 'K': 'LYS',
-                       'I': 'ILE', 'P': 'PRO', 'T': 'THR', 'F': 'PHE', 'N': 'ASN',
-                       'G': 'GLY', 'H': 'HIS', 'L': 'LEU', 'R': 'ARG', 'W': 'TRP',
-                       'A': 'ALA', 'V': 'VAL', 'E': 'GLU', 'Y': 'TYR', 'M': 'MET'}
+    GeneticCode = {"UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L",
+                   "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
+                   "UAU": "Y", "UAC": "Y", "UAA": "*", "UAG": "*",
+                   "UGU": "C", "UGC": "C", "UGA": "*", "UGG": "W",
+                   "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+                   "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+                   "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+                   "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+                   "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
+                   "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+                   "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K",
+                   "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
+                   "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+                   "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+                   "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+                   "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"}
 
-TranslationCountTable = {}
-ReverseGeneticCode = {}
+    FromThreeToOneTable = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+                           'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
+                           'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
+                           'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
 
+    FromOneToThreeTable = {'C': 'CYS', 'D': 'ASP', 'S': 'SER', 'Q': 'GLN', 'K': 'LYS',
+                           'I': 'ILE', 'P': 'PRO', 'T': 'THR', 'F': 'PHE', 'N': 'ASN',
+                           'G': 'GLY', 'H': 'HIS', 'L': 'LEU', 'R': 'ARG', 'W': 'TRP',
+                           'A': 'ALA', 'V': 'VAL', 'E': 'GLU', 'Y': 'TYR', 'M': 'MET'}
 
-def get_3_letter_code(am1):
-    if am1 in FromOneToThreeTable:
-        return FromOneToThreeTable[am1]
-    else:
-        return ''
+    TranslationCountTable = {}
+    ReverseGeneticCode = {}
+    TranslationCountTable = {}
+    ReverseGeneticCode = {}
 
+    def __init__(self):
 
-def get_1_letter_code(am3):
-    if am3 in FromThreeToOneTable:
-        return FromThreeToOneTable[am3]
-    else:
-        return ''
+        TranslationCountTable = {}
+        ReverseGeneticCode = {}
+        TranslationCountTable = {}
+        ReverseGeneticCode = {}
 
-
-def make_reverse_code_table():
-    for codon in GeneticCode:
-        am_acid = GeneticCode[codon]
-        if am_acid not in TranslationCountTable:
-            ReverseGeneticCode[am_acid] = [codon]
-            TranslationCountTable[am_acid] = 1
-        else:
-            TranslationCountTable[am_acid] += 1
-            ReverseGeneticCode[am_acid].append(codon)
-
-    pp = pprint.PrettyPrinter(width=60, compact=True)
-    pp.pprint(TranslationCountTable)
-    pp.pprint(ReverseGeneticCode)
-
-
-def translate_protein(seq):
-    aminostring = ""
-    alen = len(seq)
-    alen = alen // 3
-
-    try:
-        for i in range(alen):
-            str1 = seq[i * 3:i * 3 + 3]
-            aa = GeneticCode[str1]
-            if aa != 'STOP':
-                aminostring += str1
+        for codon in self.GeneticCode:
+            am_acid = self.GeneticCode[codon]
+            if am_acid not in self.TranslationCountTable:
+                self.ReverseGeneticCode[am_acid] = [codon]
+                self.TranslationCountTable[am_acid] = 1
             else:
-                return aminostring
-    except:
-        pass
-        return ''
+                self.TranslationCountTable[am_acid] += 1
+                self.ReverseGeneticCode[am_acid].append(codon)
 
-    return aminostring
+
+    def three_letter_code(self, am1):
+        if am1 in self.FromOneToThreeTable:
+            return self.FromOneToThreeTable[am1]
+        else:
+            return ''
+
+
+    def one_letter_code(self, am3):
+        if am3 in self.FromThreeToOneTable:
+            return self.FromThreeToOneTable[am3]
+        else:
+            return ''
+
+
+    def get_protein(self, seq):
+        aminostring = ""
+        alen = len(seq)
+        alen = alen // 3
+
+        try:
+            for i in range(alen):
+                str1 = seq[i * 3:i * 3 + 3]
+                aa = self.GeneticCode[str1]
+                if aa != 'STOP':
+                    aminostring += aa
+                else:
+                    return aminostring
+        except:
+            pass
+            return ''
+
+        return aminostring
+
+    def get_dna(self, aminoseq):
+
+        codon_list = []
+        i = 0
+        rna_sequences = []
+
+        # First get the codons that code for each amino acids
+        for aa in aminoseq:
+            codons = self.ReverseGeneticCode[aa.upper()]
+            codon_list.append(self.ReverseGeneticCode[aa.upper()])
+            i += 1
+
+        # Then make the combinations of these codons
+        nr_aminoacids = len(codon_list)
+        combine_list = []
+        base_list = codon_list[0]
+        if nr_aminoacids == 1:
+            base_list = codon_list[0]
+        else:
+            for i in range(1, nr_aminoacids):
+                for bl in base_list:
+                    for al in codon_list[i]:
+                        new = bl + al
+                        combine_list.append(new)
+                base_list = combine_list[:]
+                combine_list = []
+
+        rna_list = base_list
+
+        # Now change the found RNA sequences into DNA sequences
+        dna_sequences = []
+        for rna in rna_list:
+            str1 = rna.replace('U', 'T')
+            dna_sequences.append(str1)
+        return dna_sequences
 
 
 def how_many_combinations(seq):
@@ -148,40 +190,28 @@ def print_long_string(sequence, width):
 
 
 if __name__ == "__main__":
-    make_reverse_code_table()
+
+    ct = CodeTable()
+    s1 = ct.one_letter_code("VAL")
+    s2 = ct.one_letter_code("SER")
+    s3 = ct.three_letter_code("Y")
+    s4 = ct.three_letter_code("W")
+    s5 = ct.get_protein('AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA')
+    s5 = ct.get_protein('GAAACU')
+    print(s5)
+    s5 = ct.get_protein('AGUUUC')
+    print(s5)
+
+    s7 = ct.get_dna('MA')
+    myseq = []
+    for dna in s7:
+        myseq.append(Seq(dna))
+        myseq.append(Seq(dna).reverse_complement())
 
 
+    print(len(s7))
 
-    print(translate_protein('AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA'))
-    # output should be MAMAPRTEINSTRING
+    pp = pprint.PrettyPrinter(width=10, compact=True)
+    pp.pprint (s7)
+    n=1
 
-    f = open("sequence_to_translate_3.txt", "r")
-    seq = f.read()
-    print_long_string(translate_protein(seq), 80)
-
-    print(f'There are {how_many_combinations("V-K-L-F-P-Y-F-N-Q-W")}, combinations for string "V-K-L-F-P-Y-F-N-Q-W"')
-
-    m = 1
-
-    s = get_3_letter_code('L')
-    s = get_1_letter_code(s)
-    s = get_3_letter_code('J')
-
-    '''
-    f = open("extra_data_set.txt", "r")
-    seq = f.read()
-
-    find_coding_sequences(seq, 'KEVFEPHYY')
-    '''
-
-    print('\n\n\n')
-    find_coding_sequences('ATGGCCATGGCCCCCAGAACTGAGATCAATAGTACCCGTATTAACGGGTGA', 'MA')
-    print('\n\n\n')
-
-    print(translate_protein('AUGGCC'))
-    print(translate_protein('GGCCAU'))
-    print(translate_protein('AUGGCC'))
-
-
-    print_long_string('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCDDD',
-                      30)
